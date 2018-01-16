d80829df8b5370a7cccc24524b74b47b2a968339
Chang Liu <cl3403@columbia.edu>
2018-01-15 23:10:06 -0500

    all changes

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def main():
     return render_template('index.html')
 
 
+@app.route('/cl3403')
+def index():
+  return render_template('mypage.html')
+
+
 if __name__ == '__main__':
     app.run()
